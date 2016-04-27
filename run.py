9367ac795b0937c16f250ba8dddae79820092ad0
David Strauss <david@davidstrauss.net>
2016-04-27 14:37:09 -0700

    Initial port from Bazaar.

---
+++ run.py
@@ -0,0 +1,21 @@
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+import pprint
+
+class POSTDoc(Resource):
+  def __init__(self):
+      Resource.__init__(self)
+
+  def render_POST(self, request):
+      pprint.pprint(request.requestHeaders)
+      print(request.content.getvalue())
+      return 'OK'
+
+  def getChild(self, name, request):
+      return self
+
+root = POSTDoc()
+factory = Site(root)
+reactor.listenTCP(8090, factory)
+reactor.run()
